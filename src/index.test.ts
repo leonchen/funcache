e8aa7283a84866d63d7afbff9cb289d77784fcc7
Leon Chen <leonhart.chen@gmail.com>
2022-05-23 16:00:20 +0800

    refactor: Refine types

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,12 @@
 import { expect } from 'chai';
 import funcache from './';
 
-const sleep = async function(ms: number) {
-  return new Promise(res => {
+type Ret = { a: string };
+
+const sleep = async function (ms: number) {
+  return new Promise((res) => {
     setTimeout(res, ms);
-  })
+  });
 };
 
 describe('funcache tests', () => {
@@ -16,38 +18,38 @@ describe('funcache tests', () => {
     };
     const cf = funcache(f, { async: false });
 
-    let res = cf('1', 1, true, {a: 1});
+    let res = cf('1', 1, true, { a: 1 });
     expect(res).to.eql('called');
     expect(called).to.eql(1);
-    res = cf('1', 1, true, {a: 1});
+    res = cf('1', 1, true, { a: 1 });
     expect(res).to.eql('called');
     expect(called).to.eql(1);
 
-    res = cf('a', 1, true, {a: 1});
+    res = cf('a', 1, true, { a: 1 });
     expect(res).to.eql('called');
     expect(called).to.eql(2);
-    res = cf('a', 1, true, {a: 1});
+    res = cf('a', 1, true, { a: 1 });
     expect(res).to.eql('called');
     expect(called).to.eql(2);
 
-    res = cf('1', 2, true, {a: 1});
+    res = cf('1', 2, true, { a: 1 });
     expect(res).to.eql('called');
     expect(called).to.eql(3);
-    res = cf('1', 2, true, {a: 1});
+    res = cf('1', 2, true, { a: 1 });
     expect(res).to.eql('called');
     expect(called).to.eql(3);
 
-    res = cf('a', 1, false, {a: 1});
+    res = cf('a', 1, false, { a: 1 });
     expect(res).to.eql('called');
     expect(called).to.eql(4);
-    res = cf('a', 1, false, {a: 1});
+    res = cf('a', 1, false, { a: 1 });
     expect(res).to.eql('called');
     expect(called).to.eql(4);
 
-    res = cf('a', 1, false, {a: 0});
+    res = cf('a', 1, false, { a: 0 });
     expect(res).to.eql('called');
     expect(called).to.eql(5);
-    res = cf('a', 1, false, {a: 0});
+    res = cf('a', 1, false, { a: 0 });
     expect(res).to.eql('called');
     expect(called).to.eql(5);
   });
@@ -64,10 +66,10 @@ describe('funcache tests', () => {
       cacheAgeGetter: (res: string) => {
         if (res === '') {
           return -1;
-        } else  {
+        } else {
           return 100;
         }
-      }
+      },
     });
 
     let res = cf('');
@@ -95,8 +97,6 @@ describe('funcache tests', () => {
   });
 
   it('should work for async functions', async () => {
-    type Ret = { a: string };
-
     let called: number = 0;
 
     const f = async function (a: string): Promise<Ret> {
@@ -104,7 +104,7 @@ describe('funcache tests', () => {
       await sleep(100);
       if (a === '') throw new Error('error');
       return { a };
-    }
+    };
 
     const cf: (a: string) => Promise<Ret> = funcache(f, {
       primitive: true,
@@ -128,5 +128,102 @@ describe('funcache tests', () => {
     }
     expect(catched).to.eql(true);
     expect(called).to.eql(3);
+  });
+
+  it('should work for class methods', async () => {
+    class TestClass {
+      called: number = 0;
+      asyncCalled: number = 0;
+
+      run = funcache(
+        (s: string): string => {
+          this.called += 1;
+          return s;
+        },
+        {
+          primitive: true,
+          cacheAgeGetter: (res: string) => {
+            if (res === '') {
+              return -1;
+            } else {
+              return 100;
+            }
+          },
+        }
+      );
+
+      asyncRun = funcache(
+        async (a: string): Promise<Ret> => {
+          this.asyncCalled += 1;
+          await sleep(100);
+          if (a === '') throw new Error('error');
+          return { a };
+        },
+        {
+          async: true,
+          primitive: true,
+          cacheAgeGetter: (res: Ret) => {
+            if (res.a === '1') {
+              return -1;
+            } else {
+              return 100;
+            }
+          },
+        }
+      );
+    }
+
+    const tc = new TestClass();
+
+    // sync method
+    let res = tc.run('');
+    expect(res).to.eql('');
+    expect(tc.called).to.eql(1);
+
+    await sleep(10);
+    res = tc.run('');
+    expect(res).to.eql('');
+    expect(tc.called).to.eql(2);
+
+    res = tc.run('a');
+    expect(res).to.eql('a');
+    expect(tc.called).to.eql(3);
+
+    await sleep(90);
+    res = tc.run('a');
+    expect(res).to.eql('a');
+    expect(tc.called).to.eql(3);
+
+    await sleep(20);
+    res = tc.run('a');
+    expect(res).to.eql('a');
+    expect(tc.called).to.eql(4);
+
+    // async method
+    let asyncRes = await tc.asyncRun('a');
+    expect(asyncRes.a).to.eql('a');
+    expect(tc.asyncCalled).to.eql(1);
+    asyncRes = await tc.asyncRun('a');
+    expect(asyncRes.a).to.eql('a');
+    expect(tc.asyncCalled).to.eql(1);
+    asyncRes = await tc.asyncRun('b');
+    expect(asyncRes.a).to.eql('b');
+    expect(tc.asyncCalled).to.eql(2);
+
+    let catched = false;
+    try {
+      asyncRes = await tc.asyncRun('');
+    } catch (e) {
+      catched = true;
+    }
+    expect(catched).to.eql(true);
+    expect(tc.asyncCalled).to.eql(3);
+
+    asyncRes = await tc.asyncRun('1');
+    expect(asyncRes.a).to.eql('1');
+    expect(tc.asyncCalled).to.eql(4);
+    asyncRes = await tc.asyncRun('1');
+    expect(asyncRes.a).to.eql('1');
+    expect(tc.asyncCalled).to.eql(5);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,59 @@
 import * as LRU from 'lru-cache';
 import * as uuidv5 from 'uuid/v5';
 
-export type PromiseInfer<T> = T extends Promise<infer R> ? R : T
+export type PromiseInfer<T> = T extends Promise<infer R> ? R : T;
 
 export interface Options<T> {
   max?: number;
   async?: boolean;
-  cacheAgeGetter?: (res: PromiseInfer<T>) => number;
+  cacheAgeGetter?: (res: T) => number;
   primitive?: boolean;
+  cacheNamespace?: string;
 }
 
-export type Func<T> = ((...args: any[]) => T) | ((...args: any[]) => Promise<PromiseInfer<T>>);
+export type SyncFunc<T> = (...args: any[]) => T;
+export type AsyncFunc<T> = (...args: any[]) => Promise<T>;
+export type Func<T> = SyncFunc<T> | AsyncFunc<T>;
 
 export const CACHE_KEY_NAMESPACE = '2a848cdb-6acd-48d5-ac74-970972c1038e';
 export const KEY_JOINER = '\u200d';
 
-function getCacheKey (args: any[], primitive?: boolean): string {
+function getCacheKey(args: any[], primitive: boolean, namespace: string): string {
   if (primitive) return args.join(KEY_JOINER);
   const signature: string[] = [];
   for (const a of args) {
     signature.push(JSON.stringify(a));
   }
-  return uuidv5(signature.join(KEY_JOINER), CACHE_KEY_NAMESPACE);
+  return uuidv5(signature.join(KEY_JOINER), namespace);
 }
 
-export default function funcache<T>(f: Func<T>, opts: Options<T>): Func<T> {
-  const { max, async, primitive, cacheAgeGetter } = opts;
-  const cache = new LRU({ max});
+export default function funcache<T, R extends Func<T>>(f: R, opts: Options<T>): R {
+  const {
+    max,
+    async = false,
+    primitive = false,
+    cacheAgeGetter,
+    cacheNamespace = CACHE_KEY_NAMESPACE,
+  } = opts;
+  const cache = new LRU({ max });
 
   if (async) {
-    return (async function(...args: any[]) {
-      const key = getCacheKey(args, primitive);
+    return async function (...args: any[]) {
+      const key = getCacheKey(args, primitive, cacheNamespace);
       if (cache.has(key)) return cache.get(key);
-      const res = await f(...args) as PromiseInfer<T>;
+      const res = (await f(...args)) as T;
       const age = cacheAgeGetter ? cacheAgeGetter(res) : 0;
       cache.set(key, res, age);
       return res;
-    }) as Func<T>;
+    } as R;
   }
 
-  return (function(...args: any[]) {
-    const key = getCacheKey(args, primitive);
+  return function (...args: any[]) {
+    const key = getCacheKey(args, primitive, cacheNamespace);
     if (cache.has(key)) return cache.get(key);
-    const res = f(...args) as PromiseInfer<T>;
+    const res = f(...args) as T;
     const age = cacheAgeGetter ? cacheAgeGetter(res) : 0;
     cache.set(key, res, age);
     return res;
-  }) as Func<T>;
+  } as R;
 }
